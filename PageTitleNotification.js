5a2b8bee9b96651945c12e43a526d3d916329c7c
CurtTimson <curt@live.co.uk>
2015-11-04 21:23:53 +0000

    Initial commit of PageTitleNotification.js

---
+++ PageTitleNotification.js
@@ -0,0 +1,18 @@
+﻿var PageTitleNotification = {
+    Vars: {
+        OriginalTitle: document.title,
+        Interval: null
+    },
+    On: function (notification, intervalSpeed) {
+        var _this = this;
+        _this.Vars.Interval = setInterval(function () {
+            document.title = (_this.Vars.OriginalTitle == document.title)
+                                ? notification
+                                : _this.Vars.OriginalTitle;
+        }, (intervalSpeed) ? intervalSpeed : 1000);
+    },
+    Off: function () {
+        clearInterval(this.Vars.Interval);
+        document.title = this.Vars.OriginalTitle;
+    }
+};
