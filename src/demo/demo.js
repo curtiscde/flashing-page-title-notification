df37a8862900d496f045825d77d74d6af4874733
Curtis Timson <curt@live.co.uk>
2019-06-26 17:02:56 +0100

    Remove jQuery dependency from demo

---
+++ src/demo/demo.js
@@ -0,0 +1,10 @@
+﻿var btnOn = document.getElementById('btn-on');
+var btnOff = document.getElementById('btn-off');
+
+btnOn.addEventListener('click', function(){
+    pageTitleNotification.on("test", 1000);
+});
+
+btnOff.addEventListener('click', function(){
+    pageTitleNotification.off();
+});
